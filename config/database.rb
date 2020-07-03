17a0221fae592e420c3ec197870204ec6b936a53
sierrarose1990 <sierrarosedavenport@gmail.com>
2020-07-03 02:51:38 +0000

    initial commit

--- config/database.rb
+++ config/database.rb
@@ -3,17 +3,27 @@ configure do
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
-
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
-
+  if Sinatra::Application.development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db_url = 'postgres://[your_postgres_url here]'
+    db = URI.parse(ENV['DATABASE_URL'] || db_url)
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
-
 end
